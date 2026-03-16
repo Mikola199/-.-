4738da3ba1254a992609434a9bcab3bae1b04877
Mikola199 <91286768+Mikola199@users.noreply.github.com>
2026-03-16 17:49:51 +0300

    Update next-env.d.ts with GitHub Actions setup

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,4 +1,7 @@
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
+     - name: Checkout
+      uses: actions/checkout@2490be948fa58c5f768291a661ab6c5c5477c8d9
 
-// NOTE: This file should not be edited
+    - name: Set up Python
+      uses: actions/setup-python@b41fa79e51c0baff596db372b7b57d901d89cf65
+      with:
+        python-version: "3.11"
